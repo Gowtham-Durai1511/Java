9078610d21bb7f4d5d13aa13fe8809b7ecb61af8
Gowtham-Durai1511 <gowthamd151103@gmail.com>
2024-06-26 18:18:10 +0530

    Add files via upload

---
+++ minimum.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class minimum {
+    public static void main(String[] args) {
+        LinkedList <String> program =new LinkedList<>();{
+            Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+            int arr[]=new int[a];
+            for( int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+            }
+            int min=arr[0];
+              for( int i=1;i<arr.length;i++){
+                if(arr[i]<min){
+                    min=arr[i];
+                }
+
+              }
+              System.out.println("Minimum value is :" +min);
+           
+          }
+        }    
+     
+    }
+
